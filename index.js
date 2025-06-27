3dc4cb2e20387d7b5fcf14fbb43f733fd16c751b
mayouspublishing <admin@mayous.org>
2025-06-27 16:49:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ addEventListener('fetch', event => {
 async function handleRequest(request) {
   const url = new URL(request.url)
 
-  // Only redirect paths that start with /blog/
-  if (url.pathname.startsWith('/blog/')) {
-    const targetPath = url.pathname.replace(/^\/blog\/?/, '/')
-    const targetUrl = `https://blog.mayous.org${targetPath}${url.search}`
+  // Redirect /blog and /blog/ to blog.mayous.org/
+  if (url.pathname === '/blog' || url.pathname === '/blog/') {
+    return Response.redirect('https://blog.mayous.org/', 301)
+  }
 
+  // Redirect /blog/* to blog.mayous.org/*
+  if (url.pathname.startsWith('/blog/')) {
+    const targetPath = url.pathname.replace('/blog/', '/')
+    const targetUrl = `https://blog.mayous.org${targetPath}${url.search}`
     return Response.redirect(targetUrl, 301)
   }
 
-  // If not matching /blog/, just fetch original
+  // For everything else, serve normally
   return fetch(request)
 }
